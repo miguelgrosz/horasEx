561430e31e83a00ccac82f30c79f5dc4460fb44e
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-05-23 20:22:43 -0300

    EX

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("------Lucro Extras(Mês)------");
+
+Console.WriteLine("--------Salário por Hora--------");
+decimal salarioHora = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("------Horas Trabalhadas------");
+decimal horasTrabalhadas = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("------Horas Extras------");
+decimal horasExtras = Convert.ToDecimal(Console.ReadLine());
+
+decimal salarioBase = salarioHora * horasTrabalhadas;
+decimal valorHoraExtra = salarioHora * 1.4m;
+decimal salarioHorasExtras = valorHoraExtra * horasExtras;
+
+decimal salarioTotal = salarioBase + salarioHorasExtras;
+
+Console.WriteLine("Seu Ganho Total foi: R$ " + salarioTotal.ToString("F2"));
